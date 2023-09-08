900feb72833647a7be7284c2a9ee55925b896c4b
Vinit Vyas <vinitvyas09@vinits-mbp.attlocal.net>
2023-09-07 20:14:36 -0700

    Add Route groups. Update global.css and layout.tsx

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return(
+        <p>Dashboard Page (Protected)</p>
+    )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return(
+        <div>
+            <p> Landing Page! (Unprotected)</p>
+        </div>
+    )
+}
+
+export default LandingPage;
