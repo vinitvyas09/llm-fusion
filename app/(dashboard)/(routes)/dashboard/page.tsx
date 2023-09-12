1f2bab040a4f84885766ce3127cf50268d5747c3
Vinit Vyas <vinitvyas09@vinits-mbp.attlocal.net>
2023-09-11 20:54:16 -0700

    Create Navbar component and enable it

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
     return(
         <div>
             <p>Dashboard Page (Protected)</p>
-
-            <UserButton afterSignOutUrl="/"/>
         </div>
     )
 }
