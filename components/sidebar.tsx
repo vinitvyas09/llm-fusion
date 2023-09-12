c9c9820a9386953433b112e3f88ae6a3c80a1415
Vinit Vyas <vinitvyas09@vinits-mbp.attlocal.net>
2023-09-11 23:16:52 -0700

    Implement all Sidebar Components

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,69 @@
 "use client"
 
-import { cn } from "@/lib/utils";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { 
+    Code,
+    ImageIcon,
+    LayoutDashboard, 
+    MessageSquare, 
+    MusicIcon, 
+    Settings, 
+    Settings2, 
+    VideoIcon
+} from "lucide-react";
 
+import { cn } from "@/lib/utils";
 const montserrat = Montserrat({
     weight: "600",
     subsets: ["latin"]
 });
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500",
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500",
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-500",
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700",
+    },
+    {
+        label: "Music Generation",
+        icon: MusicIcon,
+        href: "/music",
+        color: "text-emerald-500",
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700",
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
+        color: "text-white",
+    },
+]
 
 const Sidebar = () => {
     return(
@@ -26,6 +81,23 @@ const Sidebar = () => {
                         LLM Fusion
                     </h1>
                 </Link>
+
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link
+                            href={route.href}
+                            key={route.href}
+                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                        >
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))
+                    }
+
+                </div>
             </div>
         </div>
     );
